96a1d94a73980fa74d700d57536c33f9c83c5c63
Kanhaiya kumar <kanhaiya15399@gmail.com>
2023-06-18 14:13:01 +0530

    completed MovieList component with all functionality

--- src/MovieList.js
+++ src/MovieList.js
@@ -7,65 +7,106 @@ class MovieList extends Component {
       movies: [
         {
           title: "IRON MAN",
-          plot: "Supernatural powers shown in the movies.",
+          plot: "Iron Man is a superhero appearing in American comic books published by Marvel Comics. Co-created by writer and editor Stan Lee, developed by scripter Larry ...",
           poster:
             "https://cdn.pixabay.com/photo/2019/10/24/07/03/bluesteel-4573540_640.jpg",
-          price: 199,
-          rating: 8.9,
-          stars: 0,
+          price: 149,
+          rating: 7.9,
+          star: 0,
           fav: false,
           cart: false,
         },
         {
-          title: "BLACK WINDOW",
-          plot: "Supernatural powers shown in the movies.",
+          title: "BLACK WIDOW",
+          plot: "Black Widow is a 2021 American superhero film based on Marvel Comics featuring the character of the same name. Produced by Marvel Studios and distributed by ...",
           poster:
-            "https://cdn.pixabay.com/photo/2012/12/09/00/16/abstract-69124_640.jpg",
+            "https://cdn.pixabay.com/photo/2022/04/22/11/04/natasha-romanoff-7149465_640.png",
           price: 199,
-          rating: 8.9,
-          stars: 0,
+          rating: 8.5,
+          star: 0,
           fav: false,
           cart: false,
         },
         {
           title: "HULK",
-          plot: "Supernatural powers shown in the movies.",
+          plot: "The Hulk is a superhero appearing in American comic books published by Marvel Comics. Created by writer Stan Lee and artist Jack Kirby, the character first ...",
           poster:
             "https://cdn.pixabay.com/photo/2015/03/11/01/33/hulk-667988_640.jpg",
           price: 199,
-          rating: 8.9,
-          stars: 0,
+          rating: 6.5,
+          star: 0,
           fav: false,
           cart: false,
         },
         {
           title: "THOR",
-          plot: "Supernatural powers shown in the movies.",
+          plot: "Thor is a 2011 American superhero film based on the Marvel Comics character of the same name. Produced by Marvel Studios and distributed by Paramount ...",
           poster:
             "https://cdn.pixabay.com/photo/2019/05/24/10/09/thor-4225949_640.jpg",
           price: 199,
           rating: 8.9,
-          stars: 0,
+          star: 0,
           fav: false,
           cart: false,
         },
       ],
     };
   }
+
+  handleIncStar = (movie) => {
+    const { movies } = this.state;
+    const index = movies.indexOf(movie);
+    if (movies[index].star >= 5) return;
+    movies[index].star += 0.5;
+    this.setState({
+      movies: movies,
+    });
+  };
+  handleDecStar = (movie) => {
+    const { movies } = this.state;
+    const index = movies.indexOf(movie);
+    if (movies[index].star <= 0) return;
+    movies[index].star -= 0.5;
+    this.setState({
+      movies: movies,
+    });
+  };
+  handleFav = (movie) => {
+    const { movies } = this.state;
+    const index = movies.indexOf(movie);
+    movies[index].fav = !movies[index].fav;
+    this.setState({
+      movies: movies,
+    });
+  };
+  handleCart = (movie) => {
+    const { movies } = this.state;
+    const index = movies.indexOf(movie);
+    movies[index].cart = !movies[index].cart;
+    this.setState({
+      movies: movies,
+    });
+  };
   render() {
-    const {movies} = this.state;
+    const { movies } = this.state;
     return (
       <>
-        {movies.map((movie) => (
-          <Moviecard movie={movie} />
+        {movies.map((movie, index) => (
+          <Moviecard
+            key={index}
+            movie={movie}
+            addStar={this.handleIncStar}
+            subStar={this.handleDecStar}
+            buttonToggleFav={this.handleFav}
+            buttonToggleCart={this.handleCart}
+          />
         ))}
       </>
     );
-    
   }
 }
-Moviecard.defaultProps = {
-    title: "Hello",
-}
+// Moviecard.defaultProps = {
+//     title: "Hello",
+// }
 
 export default MovieList;
--- src/Moviecard.js
+++ src/Moviecard.js
@@ -1,47 +1,16 @@
 import { Component } from "react";
 
 class Moviecard extends Component {
-  
 
-  addStar = () => {
-    //setState form 1
-    this.setState({
-      stars: this.state.stars + 0.5,
-    });
-    // form 11
-    // this.setState((prevState) => {
-    //     return {
-    //         stars: this.state.stars + 0.5
-    //     }
-    // })
-  };
-  minusStar = () => {
-    //setState form 1
-    this.setState({
-      stars: this.state.stars - 0.5,
-    });
-  };
-  handleFav = () => {
-    this.setState({
-      fav: !this.state.fav,
-    });
-  };
-  handleCart = () => {
-    this.setState({
-      cart: !this.state.cart,
-    });
-  }
+
   render() {
-    console.log(this.props.movie);
-    const { title, plot, poster, price, rating, stars, fav, cart } = this.props.movie;
+    const {movie,addStar, subStar, buttonToggleFav, buttonToggleCart} = this.props;
+    const { title, plot, poster, price, rating, star, fav, cart } = this.props.movie;
     return (
       <div className="main">
         <div className="movie-card">
           <div className="left">
-            <img
-              src={poster}
-              alt="poster"
-            />
+            <img src={poster} alt="poster" />
           </div>
           <div className="right">
             <div className="title">{title}</div>
@@ -54,7 +23,7 @@ class Moviecard extends Component {
                   alt="decrease"
                   className="str-btn"
                   src="https://cdn-icons-png.flaticon.com/128/56/56889.png"
-                  onClick={this.minusStar}
+                  onClick={() => subStar(movie)}
                 />
                 <img
                   alt="star"
@@ -65,26 +34,38 @@ class Moviecard extends Component {
                   alt="increase"
                   className="str-btn"
                   src="https://cdn-icons-png.flaticon.com/128/3524/3524388.png"
-                  onClick={this.addStar}
+                  onClick={() => addStar(movie)}
                 />
-                <span className="starCount">{stars}</span>
+                <span className="starCount">{star}</span>
               </div>
               {fav ? (
-                <button className="unfavourite-btn" onClick={this.handleFav}>
+                <button
+                  className="unfavourite-btn"
+                  onClick={() => buttonToggleFav(movie)}
+                >
                   Un-favourite
                 </button>
               ) : (
-                <button className="favourite-btn" onClick={this.handleFav}>
+                <button
+                  className="favourite-btn"
+                  onClick={() => buttonToggleFav(movie)}
+                >
                   Favourite
                 </button>
               )}
               {cart ? (
-                <button className="cart-btn" onClick={this.handleCart}>
-                  Add to cart
+                <button
+                  className="cart-btn"
+                  onClick={() => buttonToggleCart(movie)}
+                >
+                  Remove cart
                 </button>
               ) : (
-                <button className="cart-btn" onClick={this.handleCart}>
-                  Remove cart
+                <button
+                  className="cart-btn"
+                  onClick={() => buttonToggleCart(movie)}
+                >
+                  Add to cart
                 </button>
               )}
             </div>
